8d5143d73de53d0f7b1cfae642c0bb0f491b5610
Gyanendra Kumar <gyanendrak874@gmail.com>
2024-05-03 11:44:14 +0530

    Added status code library and not implemented api

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,26 +1,32 @@
+const {StatusCodes} = require('http-status-codes');
 
-function pingProblemController(req, res){
-    res.status(200).json({message: "ping controller is up"})
+
+function pingProblemController(req, res) {
+    res.status(StatusCodes.OK).json({ message: "ping controller is up" })
 }
 
-function getProblem(req, res){
-
+function getProblem(req, res) {
+    res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not Implemented" })
 }
 
-function getProblems(req, res){
+function getProblems(req, res) {
 
+    res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not Implemented" })
 }
 
-function addProblem(req, res){
+function addProblem(req, res) {
 
+    res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not Implemented" })
 }
 
-function updateProblem(req, res){
+function updateProblem(req, res) {
 
+    res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not Implemented" })
 }
 
-function deleteProblem(req, res){
+function deleteProblem(req, res) {
 
+    res.status(StatusCodes.NOT_IMPLEMENTED).json({ message: "Not Implemented" })
 }
 
 
